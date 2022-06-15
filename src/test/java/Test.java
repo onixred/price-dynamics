815991c581f3ca6cfcf8187f2065ef2a178f1967
Andrey Maksimov <maksimov@mandarin.solutions>
2022-06-15 19:48:35 +0700

    issue #3  create skeleton new service

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,16 +5,8 @@
  */
 public class Test {
 
-    /**
-     * Вспомогающий метод заполняет массив случайными числами
-     * @param array массив
-     */
-    public static void fill(int[] array) {
-        for (int index = 0; index < array.length; index++) {
-            int newElement = (int) Math.round(100 * Math.random());
-            System.out.print(newElement + " ");
-            array[index] = newElement;
-        }
-        System.out.println();
+
+    public static void main(String[] args) {
+
     }
 }
